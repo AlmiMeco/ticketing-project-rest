3533e523118fb57c753f0745f45677dfcf17765d
Almi's PC <almimeci@gmail.com>
2023-06-05 14:09:02 -0500

    Ticketing Project Security : Project Controller
    (ProjectController methods impl)
    *createProject method not working (error 500)*

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,28 +1,64 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.ProjectService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
 
+    private final ProjectService projectService;
 
-    public ResponseEntity<ResponseWrapper> getProjects(){}
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
-    public ResponseEntity<ResponseWrapper> getProjectByCode(){}
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getProjects(){
+        return ResponseEntity.ok(new ResponseWrapper("ALL Projects Retrieved", projectService.listAllProjects(), HttpStatus.OK ));
+    }
 
-    public ResponseEntity<ResponseWrapper> createProject(){}
+    @GetMapping("/{code}")
+    public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code){
+        return ResponseEntity.ok(new ResponseWrapper("Project Retrieved by Project-Code -> " + code, projectService.getByProjectCode(code), HttpStatus.OK ));
+    }
 
-    public ResponseEntity<ResponseWrapper> updateProject(){}
+//    NOT WORKING (error 500)
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project){
+        projectService.save(project);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("New Project Created", HttpStatus.CREATED ));
+    }
 
-    public ResponseEntity<ResponseWrapper> deleteProject(){}
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project){
+        projectService.update(project);
+        return ResponseEntity.ok(new ResponseWrapper("Project -> " + project.getProjectCode() + " <- Updated", HttpStatus.OK ));
+    }
 
-    public ResponseEntity<ResponseWrapper> getProjectByManager(){}
+    @DeleteMapping("/{code}")
+    public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("code") String code){
+        projectService.delete(code);
+        return ResponseEntity.ok(new ResponseWrapper("Project -> " + code + " <- Deleted", HttpStatus.OK));
+    }
 
-    public ResponseEntity<ResponseWrapper> managerCompleteProject(){}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    @GetMapping("/manager/project-status")
+    public ResponseEntity<ResponseWrapper> getProjectByManager(){
+        return ResponseEntity.ok(new ResponseWrapper("dd", projectService.listAllProjectDetails(), HttpStatus.OK));
+    }
+
+    @PutMapping("/manager/complete/{projectCode}")
+    public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode){
+        return ResponseEntity.ok(new ResponseWrapper("Project Is Successfully Completed", HttpStatus.OK));
+    }
 
 
 }
