cbd050fc2190604ea82a4c88b384ea3d9e68e7dc
Almi's PC <almimeci@gmail.com>
2023-06-05 16:34:14 -0500

    Ticketing Project Security : KeyCloak connected
    (Security Added to ProjectController)

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
@@ -19,29 +21,34 @@ public class ProjectController {
     }
 
     @GetMapping
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjects(){
         return ResponseEntity.ok(new ResponseWrapper("ALL Projects Retrieved", projectService.listAllProjects(), HttpStatus.OK ));
     }
 
     @GetMapping("/{code}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code){
         return ResponseEntity.ok(new ResponseWrapper("Project Retrieved by Project-Code -> " + code, projectService.getByProjectCode(code), HttpStatus.OK ));
     }
 
 //    NOT WORKING (error 500)
     @PostMapping
+    @RolesAllowed({"Admin","Manager"})
     public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project){
         projectService.save(project);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("New Project Created", HttpStatus.CREATED ));
     }
 
     @PutMapping
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project){
         projectService.update(project);
         return ResponseEntity.ok(new ResponseWrapper("Project -> " + project.getProjectCode() + " <- Updated", HttpStatus.OK ));
     }
 
     @DeleteMapping("/{code}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("code") String code){
         projectService.delete(code);
         return ResponseEntity.ok(new ResponseWrapper("Project -> " + code + " <- Deleted", HttpStatus.OK));
